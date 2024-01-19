85602af6be518d57d841f33771974130b9fa30a9
ettiev <126219891+ettiev@users.noreply.github.com>
2024-01-19 13:51:47 +0200

    Provide search ability for users with JS disabled
    Add template-parts for campus, event, page, post, professor and program to be displayed in traditional search.
    Created searchform.php to make search form callable by the function get_search_form()

--- page-search.php
+++ page-search.php
@@ -51,10 +51,7 @@
     
 
       <div class="generic-content">
-            <form method="get" action="<?php echo esc_url(site_url('/')); ?>">
-                <input type="search" name="s">
-                <input type="submit" value="Search">
-            </form>
+            <?php get_search_form(); ?>
       </div>
     </div>
 
